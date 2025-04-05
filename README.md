0fac64a58b5403afb33db5dcc8023aa3379bf959
racosta <ryan.acosta@gmail.com>
2025-04-05 14:50:12 -0500

    Initial commit of Nix shell

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # monorepo
+
+## Nix
+
+In this repository, a Nix shell configuration is included in `shell.nix` which includes Bazel and
+other development tools for this repository.
+
+Nix is a prerequisite. Visit [Install Nix](https://nix.dev/install-nix.html) and use the
+instructions there. After cloning, start the Nix shell by changing into the repo root directory
+and using `nix-shell`.
---
+++ shell.nix
@@ -0,0 +1,37 @@
+{
+  ch2411 ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-24.11.tar.gz") {}
+}:
+
+ch2411.mkShell {
+  packages = with ch2411; [
+    bat
+    bazel_7
+    bazel-buildtools
+    difftastic
+    #direnv
+    eza
+    git
+    go
+    golangci-lint
+    jq
+    just
+    kondo
+    lazygit
+    mdcat
+    pre-commit
+    python3
+    ripgrep
+    starship
+    tokei
+    uv
+    vim
+  ];
+
+  GREETING = "Hello, Nix!";
+
+  shellHook = ''
+    alias ls='eza --icons'
+
+    eval "$(starship init bash)"
+  '';
+}
