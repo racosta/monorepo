b8c5f1e1a6a0f1d8c0a3e469730843c1061becd9
racosta <ryan.acosta@gmail.com>
2025-04-14 18:43:32 -0500

    Add aspect rules_py and work through an example adding a test

--- projects/python_calculator/calculator.py
+++ projects/python_calculator/calculator.py
@@ -1,3 +1,11 @@
 class Calculator:
-    def add(self, x, y):
+    def add(self, x: int, y: int) -> int:
+        """Adds two integers together.
+
+        Args:
+            x (int): First addend
+            y (int): Second addend
+
+        Returns:
+            int: Sum"""
         return x + y
