1c072532278dd97c6145d6dbb98f795283ba9608
Ryan Acosta <racosta@users.noreply.github.com>
2025-12-19 12:04:18 -0600

    chore: switch from Nix shell to devenv (#19)
    * chore: switch from Nix shell to devenv
    
    * chore: update GitHub actions to use devenv instead of Nix
    
    * chore: create ci profile for devenv with fewer packages
    
    * chore: use devenv profiles that I actually tested locally
    
    * chore: slim the ci profile down further

---
+++ devenv.nix
@@ -0,0 +1,71 @@
+{ pkgs, lib, config, ... }:
+
+{
+  # 1. Define the "base" configuration (optional - common to all)
+  # Packages used everywhere (Dev + CI)
+  packages = with pkgs; [
+    bazel_8
+    bazel-buildtools
+  ];
+
+  # 2. Define the Profiles
+  profiles = {
+    # The 'ci' profile: only adds what's strictly necessary for the runner
+    ci.module = {
+      packages = with pkgs; [
+        lcov
+      ];
+      # You can even disable expensive checks in CI if needed
+      git-hooks.hooks.shellcheck.enable = false;
+    };
+
+    # The 'dev' profile: adds interactive tools and quality-of-life packages
+    dev.module = {
+      packages = with pkgs; [
+        bat
+        difftastic
+        dua
+        eza
+        git
+        go
+        golangci-lint
+        jq
+        just
+        kondo
+        lazygit
+        lcov
+        mdcat
+        openssl
+        pre-commit
+        python3
+        readline
+        ripgrep
+        starship
+        tokei
+        uv
+        vim
+        zlib
+      ];
+
+      enterShell = ''
+        alias ls='eza --icons'
+        eval "$(starship init bash)"
+      '';
+    };
+  };
+
+  # Global environment variables
+  env.GREETING = "Hello, Nix!";
+
+  # # Enable devenv's built‑in pre-commit integration
+  # pre-commit = {
+  #   enable = true;
+
+  #   # devenv will automatically install hooks from .pre-commit-config.yaml
+  #   # and run them on `git commit`.
+  #   hooks = {
+  #     trailing-whitespace.enable = true;
+  #     end-of-file-fixer.enable = true;
+  #   };
+  # };
+}
---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Development environment using devenv, equivalent to the original shell.nix";
+
+  inputs = {
+    nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/nixos-25.11.tar.gz";
+    devenv.url = "github:cachix/devenv";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, devenv, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = devenv.lib.mkShell {
+          inherit pkgs;
+          modules = [ ./devenv.nix ];
+        };
+      });
+}
