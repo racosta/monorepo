234838a1b0b3e1006e139dbaf9de606537ffb50a
Ryan Acosta <racosta@users.noreply.github.com>
2025-12-23 10:04:59 -0600

    refactor: update hello_world for rust based on learn-go-with-tests (#35)

--- projects/rust_learn_rust_with_tests/hello_world/src/main.rs
+++ projects/rust_learn_rust_with_tests/hello_world/src/main.rs
@@ -1,41 +1,78 @@
+#[allow(dead_code)]
 fn main() {
   println!("Hello, world!");
 
-  let hello = hello_world();
-  println!("{}", hello);
+  let hello_world = hello_world();
+  println!("{}", hello_world);
 
-  let name = String::from("Rusty");
-  greeting(name);
+  hello("Rusty", "");
 }
 
-fn hello_world() -> String {
-  let greeting = String::from("Hello, World!");
-  greeting
+pub const SPANISH_LANGUAGE: &str = "Spanish";
+pub const FRENCH_LANGUAGE: &str = "French";
+
+pub const ENGLISH_HELLO_PREFIX: &str = "Hello, ";
+pub const SPANISH_HELLO_PREFIX: &str = "Hola, ";
+pub const FRENCH_HELLO_PREFIX: &str = "Bonjour, ";
+
+pub fn hello(name: &str, language: &str) -> String {
+  let prefix = greeting_prefix(language);
+  let final_name = name.is_empty().then_some("World").unwrap_or(&name);
+  format!("{prefix}{final_name}!")
 }
 
-fn greeting(name: String) -> String {
-  let hello = String::from("Hello, ");
-  let greeting = format!("{hello}{name}!");
-  greeting
+pub fn greeting_prefix(language: &str) -> String {
+  match language {
+    SPANISH_LANGUAGE => String::from(SPANISH_HELLO_PREFIX),
+    FRENCH_LANGUAGE => String::from(FRENCH_HELLO_PREFIX),
+    _ => String::from(ENGLISH_HELLO_PREFIX),
+  }
+}
+
+pub fn hello_world() -> String {
+  hello("World", "English")
 }
 
 #[cfg(test)]
 mod tests {
-  use super::greeting;
+  use super::hello;
   use super::hello_world;
+  use super::FRENCH_LANGUAGE;
+  use super::SPANISH_LANGUAGE;
 
   #[test]
-  fn hello_world_test() {
+  fn test_hello_world() {
     let want = String::from("Hello, World!");
-    let result = hello_world();
-    assert_eq!(want, result);
+    let got = hello_world();
+    assert_eq!(want, got);
   }
 
   #[test]
-  fn greeting_test() {
+  fn test_hello_to_a_person() {
     let want = String::from("Hello, Rusty!");
-    let name = String::from("Rusty");
-    let result = greeting(name);
-    assert_eq!(want, result);
+    let name = "Rusty";
+    let got = hello(name, "");
+    assert_eq!(want, got);
+  }
+
+  #[test]
+  fn test_hello_empty_string() {
+    let want = String::from("Hello, World!");
+    let got = hello("", "");
+    assert_eq!(want, got);
+  }
+
+  #[test]
+  fn test_hello_in_spanish() {
+    let want = String::from("Hola, Elodie!");
+    let got = hello("Elodie", SPANISH_LANGUAGE);
+    assert_eq!(want, got);
+  }
+
+  #[test]
+  fn test_hello_in_french() {
+    let want = String::from("Bonjour, Lauren!");
+    let got = hello("Lauren", FRENCH_LANGUAGE);
+    assert_eq!(want, got);
   }
 }
