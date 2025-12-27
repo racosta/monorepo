bc3d32280f4449e3ce8528b88c2515b40dbdf56b
Ryan Acosta <racosta@users.noreply.github.com>
2025-12-27 13:37:23 -0600

    feat: add structs sub-chapter for 100-exercises-to-learn-rust (#40)

--- projects/rust_one_hundred_exercises/02_basic_calculator/03_if_else/src/lib.rs
+++ projects/rust_one_hundred_exercises/02_basic_calculator/03_if_else/src/lib.rs
@@ -1,7 +1,7 @@
 /// Return `12` if `n` is even,
 /// `13` if `n` is divisible by `3`,
 /// `17` otherwise.
-fn magic_number(n: u32) -> u32 {
+pub fn magic_number(n: u32) -> u32 {
   if n % 2 == 0 {
     12
   } else if n % 3 == 0 {
