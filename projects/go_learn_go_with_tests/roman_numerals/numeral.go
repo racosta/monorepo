576fa171919a78198cc4d0a355e52add7218964d
racosta <ryan.acosta@gmail.com>
2026-01-03 16:19:01 -0600

    feat: add property-based-testing chapter from learn-go-with-tests

---
+++ projects/go_learn_go_with_tests/roman_numerals/numeral.go
@@ -0,0 +1,54 @@
+// Package romannumerals provides functions to convert between Arabic numbers and Roman numerals.
+package romannumerals
+
+import "strings"
+
+// A RomanNumeral represents a mapping between an Arabic value and its Roman numeral symbol.
+type RomanNumeral struct {
+	Value  uint16
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+// ConvertToRoman converts an Arabic number to its Roman numeral representation.
+func ConvertToRoman(arabic uint16) string {
+	var result strings.Builder
+
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
+	}
+
+	return result.String()
+}
+
+// ConvertToArabic converts a Roman numeral string to its Arabic number representation.
+func ConvertToArabic(roman string) uint16 {
+	var arabic = uint16(0)
+
+	for _, numeral := range allRomanNumerals {
+		for strings.HasPrefix(roman, numeral.Symbol) {
+			arabic += numeral.Value
+			roman = strings.TrimPrefix(roman, numeral.Symbol)
+		}
+	}
+
+	return arabic
+}
