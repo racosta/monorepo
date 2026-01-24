42579d954557978f87a6a21573c1ce0d69ed596f
Ryan Acosta <racosta@users.noreply.github.com>
2026-01-24 16:23:40 -0600

    feat: configure llvm toolchain for C++ (#62)
    * feat: configure llvm toolchain for C++
    
    * chore: use select code from maximize-build-space
    
    * chore: re-enable Tcl
    
    * chore: update bazel module lock

---
+++ projects/cpp_hello_world/hello_world.cpp
@@ -0,0 +1,8 @@
+#include "greeter.h"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  auto greeting = greet();
+  std::cout << greeting << std::endl;
+  return 0;
+}
---
+++ projects/cpp_hello_world_lib/greeter.h
@@ -0,0 +1,8 @@
+#ifndef PROJECTS_CPP_HELLO_WORLD_LIB_GREETER_H__
+#define PROJECTS_CPP_HELLO_WORLD_LIB_GREETER_H__
+
+#include <string>
+
+std::string greet(const std::string& name = "World");
+
+#endif  // PROJECTS_CPP_HELLO_WORLD_LIB_GREETER_H__
