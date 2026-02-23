b88f56bd13c85c44dda28c3f96a551e7e86413a1
racosta <ryan.acosta@gmail.com>
2026-02-22 18:43:52 -0600

    feat: add math chapter of learn-go-with-tests up to Draw the Clock

---
+++ projects/go/learn_go_with_tests/math/clockface.go
@@ -0,0 +1,39 @@
+// Package clockface provides functions to calculate the position of the hands of an analogue clock.
+package clockface
+
+import (
+	"math"
+	"time"
+)
+
+// A Point represents a two-dimensional Cartesian coordinate
+type Point struct {
+	X float64
+	Y float64
+}
+
+const secondHandLength = 90
+const clockCenterX = 150
+const clockCenterY = 150
+
+// SecondHand is the unit vector of the second hand of an analogue clock at time `t`
+// represented as a Point.
+func SecondHand(t time.Time) Point {
+	p := secondHandPoint(t)
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength}
+	p = Point{p.X, -p.Y}
+	p = Point{p.X + clockCenterX, p.Y + clockCenterY}
+	return p
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return math.Pi / (30 / float64(t.Second()))
+}
+
+func secondHandPoint(t time.Time) Point {
+	angle := secondsInRadians(t)
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+
+	return Point{x, y}
+}
