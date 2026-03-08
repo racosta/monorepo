67ff3538a2304e26c30c4c86db5ad49893e6c842
Ryan Acosta <racosta@users.noreply.github.com>
2026-03-08 12:04:38 -0500

    feat: add spdlog dep and update C++ hello world example (#86)

--- projects/cpp/hello_world/hello_world.cpp
+++ projects/cpp/hello_world/hello_world.cpp
@@ -1,8 +1,21 @@
 #include "greeter.h"
-#include <iostream>
+#include "spdlog/cfg/argv.h"
+#include "spdlog/cfg/env.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/spdlog.h"
 
 int main(int argc, char *argv[]) {
+  // SPDLOG_LEVEL=error,console=info
+  spdlog::cfg::load_env_levels();
+  // ./hello_world SPDLOG_LEVEL=error,console=info
+  spdlog::cfg::load_argv_levels(argc, argv);
+
+  auto console = spdlog::stdout_color_mt("console");
+
   auto greeting = greet();
-  std::cout << greeting << std::endl;
+
+  spdlog::info(greeting);
+  console->info(greeting);
+
   return 0;
 }
