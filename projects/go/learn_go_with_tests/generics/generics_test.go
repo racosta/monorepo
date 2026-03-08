c2d3e517b30db9ef77b9e5850a404094d22d82bb
Ryan Acosta <racosta@users.noreply.github.com>
2026-03-07 18:17:23 -0600

    feat: add generics chapter of learn-go-with-tests (#85)

---
+++ projects/go/learn_go_with_tests/generics/generics_test.go
@@ -0,0 +1,55 @@
+package generics
+
+import "testing"
+
+func TestAssertFunctions(t *testing.T) {
+	t.Run("asserting on integers", func(t *testing.T) {
+		AssertEqual(t, 1, 1)
+		AssertNotEqual(t, 1, 2)
+	})
+
+	t.Run("asserting on strings", func(t *testing.T) {
+		AssertEqual(t, "hello", "hello")
+		AssertNotEqual(t, "hello", "Grace")
+	})
+}
+
+func TestStack(t *testing.T) {
+	t.Run("integer stack", func(t *testing.T) {
+		myStackOfInts := NewStack[int]()
+
+		// check stack is empty
+		AssertTrue(t, myStackOfInts.IsEmpty())
+
+		// add a thing, then check it's not empty
+		myStackOfInts.Push(123)
+		AssertFalse(t, myStackOfInts.IsEmpty())
+
+		// add another thing, pop it back again
+		myStackOfInts.Push(456)
+		value, _ := myStackOfInts.Pop()
+		AssertEqual(t, value, 456)
+		value, _ = myStackOfInts.Pop()
+		AssertEqual(t, value, 123)
+		AssertTrue(t, myStackOfInts.IsEmpty())
+	})
+
+	t.Run("string stack", func(t *testing.T) {
+		myStackOfStrings := NewStack[string]()
+
+		// check stack is empty
+		AssertTrue(t, myStackOfStrings.IsEmpty())
+
+		// add a thing, then check it's not empty
+		myStackOfStrings.Push("123")
+		AssertFalse(t, myStackOfStrings.IsEmpty())
+
+		// add another thing, pop it back again
+		myStackOfStrings.Push("456")
+		value, _ := myStackOfStrings.Pop()
+		AssertEqual(t, value, "456")
+		value, _ = myStackOfStrings.Pop()
+		AssertEqual(t, value, "123")
+		AssertTrue(t, myStackOfStrings.IsEmpty())
+	})
+}
