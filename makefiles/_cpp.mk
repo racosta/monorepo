3bffa9bbb97bcb488741c542b8fca0c14141054e
racosta <ryan.acosta@gmail.com>
2026-04-14 21:36:57 -0500

    chore: replace openssl with boringssl

--- makefiles/_cpp.mk
+++ makefiles/_cpp.mk
@@ -1,3 +1,17 @@
 REPO_ROOT := $(shell git rev-parse --show-toplevel)
 
 include $(REPO_ROOT)/makefiles/_common.mk
+
+.PHONY: all build run
+
+all: build ## Default target (build)
+
+build: ## Build the first C++ binary
+	bazel query "kind('cc_binary', ...)" --output=label | \
+		head -n 1 | \
+		xargs bazel build
+
+run: ## Run first C++ binary
+	bazel query "kind('cc_binary', ...)" --output=label | \
+		head -n 1 | \
+		xargs bazel run
--- projects/cpp/openssl/main.cpp
+++ projects/cpp/openssl/main.cpp
@@ -1,24 +1,16 @@
-#include "nlohmann/json.hpp"
 #include "spdlog/cfg/argv.h"
 #include "spdlog/cfg/env.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/spdlog.h"
-
-using json = nlohmann::json;
+#include <openssl/crypto.h>   // Include for OpenSSL_version() function
+#include <openssl/opensslv.h> // Include OpenSSL version header
 
 int main(int argc, char *argv[]) {
   // SPDLOG_LEVEL=error
   spdlog::cfg::load_env_levels();
-  // ./nlohmann_json SPDLOG_LEVEL=error
+  // ./rapidjson SPDLOG_LEVEL=error
   spdlog::cfg::load_argv_levels(argc, argv);
 
-  json j;
-  j["project"] = "Bazel Example";
-  j["library"] = "nlohmann/json";
-  j["version"] = "3.12.0";
-  j["status"] = "working";
-
-  spdlog::info("{}", j.dump(2));
-
+  spdlog::info("OpenSSL Version: {}", OpenSSL_version(OPENSSL_VERSION));
   return 0;
 }
