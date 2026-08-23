0d77e8c863a25f59951277e71caafcd75fe43386
racosta <ryan.acosta@gmail.com>
2026-08-23 15:31:40 -0500

    feat: Add Python FastAPI example
    Add Python FastAPI example

---
+++ projects/python/fastapi/tests/test_read_root.py
@@ -0,0 +1,14 @@
+"""Test the root endpoint of the FastAPI application."""
+
+from fastapi.testclient import TestClient
+
+
+def test_read_root(client: TestClient):
+    """Test the root endpoint of the FastAPI application.
+
+    Args:
+        client (TestClient): The FastAPI test client fixture.
+    """
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
